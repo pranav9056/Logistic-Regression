e331cc2532f300e69031616e3faa82146a927395
Pranav <pranavjain3194@gmail.com>
2018-05-25 18:56:30 -0400

    added costFunction and predict

--- costFunction.m
+++ costFunction.m
@@ -6,27 +6,10 @@ function [J, grad] = costFunction(theta, X, y)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
-
-
-
-
-
-
-
-
-% =============================================================
-
+thetaX = X * theta;
+predicted = sigmoid(thetaX);
+J = -1/m * sum(y .* log(predicted) + (1-y) .* log(1-predicted));
+grad = sum((predicted - y) .* X);
+grad = grad';
+grad = grad ./ m;
 end
