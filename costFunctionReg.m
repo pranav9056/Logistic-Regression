31f00806ee57c80cff3fba5836bf2695813318d6
Pranav <pranavjain3194@gmail.com>
2018-05-25 21:52:14 -0400

    added regularization

--- costFunctionReg.m
+++ costFunctionReg.m
@@ -8,20 +8,14 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-
-
-
-
-
-
-% =============================================================
-
+thetaX = X * theta;
+predicted = sigmoid(thetaX);
+J = -1/m * sum(y .* log(predicted) + (1-y) .* log(1-predicted));
+J = J + (lambda/(2*m))* sum(theta(2:end) .^ 2);
+grad = sum((predicted - y) .* X);
+grad = grad';
+grad = grad ./ m;
+gradRegularizer = lambda/m .* theta;
+gradRegularizer(1) = 0;
+grad = grad + gradRegularizer;
 end
